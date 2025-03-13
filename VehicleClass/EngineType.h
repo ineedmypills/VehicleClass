7facfea964aed919d0a316cbd2cc8461b7626500
ElNefro <se1fa1@mail.ru>
2025-03-13 09:27:07 +0500

    тванфорт

---
+++ VehicleClass/EngineType.h
@@ -0,0 +1,5 @@
+#pragma once
+enum class EngineType
+{
+	V4, V6, V8, V12, V16
+};
--- VehicleClass/Vehicle.h
+++ VehicleClass/Vehicle.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "EngineType.h"
+#include <iostream>
+#include <Windows.h>
+
+using enum EngineType;
 class Vehicle
 {
-};
+public:
+	Vehicle(double gas, int wheelsAmount, enum engineType);
+	virtual ~Vehicle() = default;
+	
+	virtual void Beep();
+	virtual void Refuel();
+	virtual void StartEngine();
 
+private:
+	double gas;
+	int wheelsAmount;
+	enum engineType;
+};
